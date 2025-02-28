32dc2ba3615fad7e318d8aeccbd901e965003760
KimJY <sagwai141@gmail.com>
2025-03-01 02:01:18 +0900

    Add files via upload

---
+++ tacticLive/tacticLive/src/main/java/websocket/website/TacticLiveApplication.java
@@ -0,0 +1,13 @@
+package websocket.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacticLiveApplication.class, args);
+	}
+
+}
---
+++ tacticLive/tacticLive/src/test/java/websocket/website/tacticLive/TacticLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package websocket.website.tacticLive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacticLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
